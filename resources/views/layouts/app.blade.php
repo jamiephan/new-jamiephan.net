bdb0c27c57b3165d95984985255a7adf2ea4c225
Jamie Phan <admin@jamiephan.net>
2018-02-18 19:41:41 +0000

    Re-Included JS file, exlude vue for now

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,6 @@
         @endif
          @yield("content")
     </div>
-    {{--  <script src="{{asset('js/app.js')}}"></script>  --}}
+    <script src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
